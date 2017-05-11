0f776c11c0eef761a3654147a715f0425fe324a2
M. Emre Aydın <emraydin@gmail.com>
2017-05-11 22:47:00 +0200

    test codes of projecting a circle onto the 19th Face (which turned out to be wrong). This commit also uses the right orientation for the vertices while drawing them

--- visualize.py
+++ visualize.py
@@ -27,7 +27,12 @@ def draw_face(points, face):
 
     # the line below might not work, but we need to add [0.0, 0.0] to the very end of the numpy matrix
     vertices = face.calcLocalVertices()
-    vertices.append(np.array([0.0, 0.0]))
+    new_vertices = []
+    for v in vertices:
+        new_vertices.append(np.array([-v[1], -v[0]]))
+    new_vertices.append(np.array([0.0, 0.0]))
+
+
 
     # this is the mask to draw the triangle. the last line actually closes the poly
     codes = [Path.MOVETO,
@@ -35,10 +40,12 @@ def draw_face(points, face):
              Path.LINETO,
              Path.CLOSEPOLY,
          ]
-    path = Path(vertices, codes)
+    path = Path(new_vertices, codes)
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
     patch = patches.PathPatch(path, facecolor='white', lw=1, alpha=0.5)
     ax.add_patch(patch)
-    ax.scatter(points[:,0], points[:,1])
+    ax.scatter(-points[:,1], -points[:,0])
+
+    plt.show()
