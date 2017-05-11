d177f1caf70d9e36475d53fb0afd77950527b227
David Gardenier <davidgardenier@gmail.com>
2017-05-11 22:31:28 +0200

    Allow of scaling

--- plot.py
+++ plot.py
@@ -6,7 +6,7 @@ size = 5
 xs = random.random(2)
 ys = random.random(2)
 mags = [-1.44, 10.71]
-mags = [-mag+12.15 for mag in mags]
+mags = [(-mag+12.15)*5 for mag in mags]
 
 plt.scatter(xs, ys, s=mags, c='k')
 plt.axis('off')
