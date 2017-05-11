64ac9e77335052e3f13c925099bfa8cbf0f1bf1c
David Gardenier <davidgardenier@gmail.com>
2017-05-11 21:40:03 +0200

    Update input

--- import_stars.py
+++ import_stars.py
@@ -1,17 +1,20 @@
+from collections import namedtuple
 import csv
 import math
 
+# Import data
 with open('./stars.dat', 'rb') as f:
     data = list(csv.reader(f))
 
-ra_frac = []
-dec_frac = []
+Star = namedtuple('Star', 'ra dec mag')
+stars = []
 
 for i, e in enumerate(data):
 
+    print(i)
     ra = e[0]
     dec = e[1]
-    mag = e[2]
+    m = float(e[2])
 
     if dec[1] == ' ':
         dec = dec.replace(' ', '', 1)
@@ -25,5 +28,10 @@ for i, e in enumerate(data):
         sign = 1
     dec = dd + sign*dm/60 + sign*ds/3600
 
-    ra_frac.append(math.radians(ra))
-    dec_frac.append(math.radians(dec) + math.pi/2.)
+    ra_frac = math.radians(ra)
+    dec_frac = math.radians(dec) + math.pi/2.
+
+    s = Star(ra=ra_frac, dec=dec_frac, mag=m)
+    stars.append(s)
+
+print(stars)
