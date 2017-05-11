7cd2b120308f6af8be3eca585e33b47c22bc2926
Janna112358 <jannagoldsteinmail@gmail.com>
2017-05-11 14:25:48 +0200

    edited class Face and Vertex

--- globes/projection.py
+++ globes/projection.py
@@ -1,10 +1,34 @@
 #!/usr/bin/env python
 
 
-class Surface(object):
+class Face(object):
+    """         
+    Face of a polyhedron, defined by its vertices. 
+    
+    Attributes
+    ----------
+    vertices: list
+        list of Vertex objects confining the face
+    ID: int
+        number identifying the Face
+    """
 
-    def __init__(self):
-        pass
+    def __init__(self, vertices, ID):
+        """
+        Creates a Surface Object from a set of vertices
+        
+        Parameters
+        ----------
+        vertices: list
+            list of Vertex objects confining the face
+        ID: int
+            number identifying the Face
+        """
+        
+        self.vertices = vertices
+        self.ID = ID
+    
+        
 
 
 class Vertex(object):
@@ -12,12 +36,28 @@ class Vertex(object):
     def __init__(self, x, y, z, name):
         """
         Creates the Vertex Object with Carthesian coordinates
-        :param x: x coordinate
-        :param y: y coordinate
-        :param z: z coordinate
-        :param name: The name (letter) of the Vertex
-        :return: Void
+        
+        Parameters
+        ----------
+        x: float
+            x coordinate
+        y: float
+            y coordinate
+        z: float
+            z coordinate
+        name: string
+        
+        Attributes
+        ----------
+        x: float
+            x coordinate
+        y: float
+            y coordinate
+        z: float
+            z coordinate
+        name: string
         """
+        
         self.x = x
         self.y = y
         self.z = z
