f45d30a2bf1679f28f59068db800d7e82669ffa4
Janna Goldstein <jgoldstein@star.sr.bham.ac.uk>
2017-11-15 12:11:35 +0000

    delete old version of projection

--- globes/projection.py
+++ globes/projection.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import numpy as np
+import sys
 from warnings import warn
 
 
@@ -80,6 +81,13 @@ class Face(object):
         self.numVertices = len(self.vertices)
         self.ID = ID
         
+        # check that at least three vertices are given
+        try:
+            assert(self.numVertices >= 3)
+        except AssertionError:
+            print("Can not creatre face with less than three vertices.")
+            sys.exit(1)
+        
         self.calc_system()
 
     def calc_system(self):
@@ -89,7 +97,7 @@ class Face(object):
         """        
         
         # middle as average of the vertices
-        self.middle = np.zeros(3)
+        self.middle = np.zeros(self.numVertices)
         for v in self.vertices:
             self.middle += v.coordinates
         self.middle = self.middle / self.numVertices
@@ -97,12 +105,12 @@ class Face(object):
         # normal is in the direction of the origin through the middle
         self.normal = self.middle / np.linalg.norm(self.middle)
         
-        # pick first axis from middle to first vertex (normalised)
-        # pick second axis orthogonal to first and to normal
-        self.u = self.vertices[0].coordinates - self.middle
-        self.size = np.linalg.norm(self.u)
-        self.u = self.u / np.linalg.norm(self.u)
-        self.v = np.cross(self.normal, self.u)
+        # pick frist axis from v1 to v2, second from v1 to v3
+        self.u = self.vertices[1].coordinates - self.vertices[0].coordinates
+        self.v = self.vertices[2].coordinates - self.vertices[0].coordinates
+        
+        self.angle = np.arccos(np.dot(self.u, self.v) / 
+                             (np.linalg.norm(self.u) * np.linalg.norm(self.v)))
         
     def calc_local_vertices(self):
         """
@@ -120,8 +128,8 @@ class Face(object):
         
         D = np.linalg.norm(self.middle - self.vertices[0].coordinates)
         v1 = np.array([D, 0])
-        v2 = np.array([-D*0.5, -D*0.5*np.sqrt(3.0)])
-        v3 = np.array([-D*0.5, D*0.5*np.sqrt(3.0)])
+        v2 = np.array([-D*np.cos(self.angle), -D*np.sin(self.angle)])
+        v3 = np.array([-D*np.cos(self.angle), D*np.sin(self.angle)])
         
         return [v1, v2, v3]         
     
@@ -138,13 +146,33 @@ class Face(object):
         Returns
         -------
         NumPy Array: the point in the lcs of the face
-        """
-        # check that point is in the plane of the face
-        if not np.dot((point - self.middle), self.normal) < 1.e-12:
-            warn("WARNING, point not in the place of face")
+        """        
         
-        a = np.dot(point - self.vertices[0].coordintes, self.u)
-        b = np.dot(point - self.middle, self.v)
+        eps = 1.0e-6
+        # check that point is in the plane of the face
+        assert(np.dot((point - self.middle), self.normal) < eps)
+        
+        start_vector = self.vertices[0].coordinates
+        p = point - start_vector
+        psize = np.linalg.norm(p)
+        size = np.linalg.norm(self.u)
+        
+        # calculate angles between p and basis vectors
+        # check that total angle is the same as calculated when initializing
+        cos_angle_u = np.dot(self.u, p) / (psize * size)
+        cos_angle_v = np.dot(self.v, p) / (psize * size)
+        tot_angle = np.arccos(cos_angle_u) + np.arccos(cos_angle_v)
+        cos_tangle = np.cos(self.angle)
+        assert(abs(np.cos(tot_angle) - cos_tangle) < eps)
+        
+        
+        factor = psize / (1 - cos_tangle**2.0)
+        a = factor * (cos_angle_u - np.cos(self.angle)*cos_angle_v) / size
+        b = factor * (cos_angle_v - np.cos(self.angle)*cos_angle_u) / size
+        
+        # check the lcs coordinates are within the triagle face
+        assert(a >= 0 and b >= 0)
+        assert((a + b) <= 1.0)
         return (a, b)
     
     def lcs_to_global(self, point):
@@ -154,7 +182,7 @@ class Face(object):
         p = self.middle + point[0]*self.u + point[1]*self.v
         return p
     
-    def lcs_to_net(self, point, mnet, unet, scale=1):
+    def lcs_to_net(self, point, v1net, v2net, v3net, scale=1):
         """        
         Convert coordinates in lcs to coordinates on the icosahedron net grid
         
@@ -162,11 +190,8 @@ class Face(object):
         ----------
         point: array-like
             2D coordinates of a point in the lcs of the face
-        mnet: array-like
-            coordinates of the middle point of the face in the net
-        unet: array-like
-            coordinates of the endpoint of u in the face of the net,
-            i.e. the coordinates of the first vertex on the net
+        v1net, v2net, v3net: array-like
+            2D coordinates of the vertices of the face on the net
         scale: int or float
             scale used in the icosahedron net
             default = 1
@@ -175,21 +200,16 @@ class Face(object):
         -------
         NumPy Array: coordinates of the point on the net
         """
-        # convert to numpy arrays if not arrays already
-        unet = np.array(unet)
-        mnet = np.array(mnet)
+        v1net = np.array(v1net)
+        v2net = np.array(v2net)
+        v3net = np.array(v3net)
         
-        # get the position of the endpoint of v in the net
-        Dxu = unet[0] - mnet[0]
-        Dyu = unet[1] - mnet[1]
-        vnet = np.array([mnet[0] - Dyu, mnet[1] + Dxu])
-        
-        # vectors pointing from to u and v
-        to_u = (unet - mnet) / self.size
-        to_v = (vnet - mnet) / self.size
+        # corresponding u and v vectors form the face lcs on the net
+        unet = v2net - v1net
+        vnet = v3net - v1net
         
         # coordinates of the point on the net
-        pnet = mnet + point[0]*to_u + point[1]*to_v
+        pnet = v1net + point[0]*unet + point[1]*vnet
         return pnet
         
 
@@ -260,14 +280,14 @@ class Icosahedron(object):
         """
         lowest = np.pi*2
         closest_face = self.faces[0]
-
+        
         for f in self.faces:
             pmiddle = point_to_sphere(f.middle)
-            dist = np.sqrt((pmiddle[0] - p[0])**2. + (pmiddle[1] - p[1])**2.)
+            dist = ang_distance(p, pmiddle)
             if dist < lowest:
                 lowest = dist
                 closest_face = f
-
+        
         return closest_face
     
     def project_in_3D(self, p):
@@ -312,6 +332,24 @@ class Icosahedron(object):
         face, projp3D = self.project_in_3D(p)
         return face, face.global_to_lcs(projp3D)
 
+def ang_distance(p1, p2):
+    """
+    Calculate angular distance between two point on the 1-sphere p1 and p2.
+    
+    Parameters
+    ----------
+    p1: array-like
+        point 1 in theta, phi coordinates
+    p2: array-like
+        point 2 in theta, phi coordinates
+        
+    Returns
+    -------
+    float: angular distance between p1, p2 in square radians
+    """    
+    term1 = np.sin(p1[0]) * np.sin(p2[0]) * np.cos(p1[1] - p2[1])
+    term2 = np.cos(p1[0]) * np.cos(p2[0])
+    return np.sqrt(2. - 2 * (term1 + term2))
 
 def point_to_sphere(p):
     """
